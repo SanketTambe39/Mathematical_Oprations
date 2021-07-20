780e1c14228ac4a366f9243c85d5ca3d2af2e4da
SanketTambe39 <sankettambe39@gmail.com>
2021-07-20 21:48:32 +0530

    Added : Methods to print number list using various ways

--- Mathematical_Oprations/src/main/java/mathematicaloprations/MathematicalOprations.java
+++ Mathematical_Oprations/src/main/java/mathematicaloprations/MathematicalOprations.java
@@ -1,6 +1,10 @@
 package mathematicaloprations;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 
 public class MathematicalOprations {
 
@@ -8,7 +12,7 @@ public class MathematicalOprations {
 
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter two numbers on which you want to perform the oprations ::");
-		
+
 		System.out.println("Enter Value of 1st number :: ");
 		int firstNumber = scanner.nextInt();
 		System.out.println("Enter Value of 1st number :: ");
@@ -25,13 +29,60 @@ public class MathematicalOprations {
 		System.out.println("Subtract is " + Subtract.calculate(firstNumber, secondNumber));
 		System.out.println("Multiply is " + Multiply.calculate(firstNumber, secondNumber));
 		System.out.println("Divide is " + Division.calculate(firstNumber, secondNumber));
-		
-		//passing lambda function as function parameter
-				System.out.println("\nAfter using lambda functions as function parameter");
-				MyMathFunction.printResult("Addition",firstNumber, secondNumber, add);
-				MyMathFunction.printResult("Subtract",firstNumber, secondNumber, Subtract);
-				MyMathFunction.printResult("Multiply",firstNumber, secondNumber, Multiply);
-				MyMathFunction.printResult("Divide",firstNumber, secondNumber, Division);
+
+		// passing lambda function as function parameter
+		System.out.println("\nAfter using lambda functions as function parameter");
+		MyMathFunction.printResult("Addition", firstNumber, secondNumber, add);
+		MyMathFunction.printResult("Subtract", firstNumber, secondNumber, Subtract);
+		MyMathFunction.printResult("Multiply", firstNumber, secondNumber, Multiply);
+		MyMathFunction.printResult("Divide", firstNumber, secondNumber, Division);
+
+		// creating Number list
+		List<Integer> numberList = new ArrayList<Integer>();
+		for (int i = 0; i < 6; i++) {
+			numberList.add(i);
+		}
+
+		// method 1 using iterator
+		System.out.println("\nNumbers using Iterator");
+		Iterator<Integer> it = numberList.iterator();
+		// number list using iterator
+		while (it.hasNext()) {
+			Integer integer = it.next();
+			System.out.println(integer);
+		}
+
+		// method 2 using consumer interface
+		class Myconsumer implements Consumer<Integer> {
+			public void accept(Integer t) {
+				System.out.println(t);
+			}
+		}
+
+		Myconsumer action = new Myconsumer();
+		System.out.println("\nNumbers using consumer interface");
+		numberList.forEach(action);
+
+		// method 3 Traversing using anonymous consumer
+		System.out.println("\nNumbers using anonymous consumer");
+		numberList.forEach(new Consumer<Integer>() {
+			public void accept(Integer t) {
+				System.out.println(t);
+			}
+		});
+
+		// method 4 explicit lambda function
+		System.out.println("\nNumbers using explicit lambda");
+		Consumer<Integer> numberListAction = n -> {
+			System.out.println(n);
+		};
+		numberList.forEach(numberListAction);
+
+		// method 5 implicit lambda function
+		System.out.println("\nNumbers using implicit lambda");
+		numberList.forEach(n -> {
+			System.out.println(n);
+		});
 	}
 
 }
